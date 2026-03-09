37aa4d90eabe7b9ae15514db5ea15be15d6d9408
孟松 <mengsong@58.com>
2026-03-09 14:22:21 +0800

    调整包名和包依赖

--- src/main/java/com/example/springaidemo/SpringAiDemoApplication.java
+++ src/main/java/com/example/springaidemo/SpringAiDemoApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringaidemoApplication {
+public class SpringAiDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringaidemoApplication.class, args);
+        SpringApplication.run(SpringAiDemoApplication.class, args);
+        System.out.println("启动成功");
     }
 
 }
